0cfe0385a66cea4414c3e990a7129cbacc40af20
Avery Witherow <78763092+averywit@users.noreply.github.com>
2023-12-26 18:26:50 -0500

    Cleaned up code: HI Checker

--- States/HIChecker.py
+++ States/HIChecker.py
@@ -1,15 +1,17 @@
 import requests
-import multiprocessing
+import threading
 from termcolor import colored
 from colorama import init
+from queue import Queue
 init(autoreset=True)
 
 
-class Worker(multiprocessing.Process):
+class Worker(threading.Thread):
 
-    def __init__(self, job_queue):
+    def __init__(self, job_queue, max_retries=10):
         super().__init__()
         self._job_queue = job_queue
+        self._max_retries = max_retries
 
     def run(self):
         while True:
@@ -17,33 +19,33 @@ class Worker(multiprocessing.Process):
             if word is None:
                 break
 
-            try:
-                session = requests.Session()
-                headers2 = {
-                    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-                    "accept-encoding": "gzip, deflate, br",
-                    "accept-language": "en-US,en;q=0.9",
-                    "cache-control": "max-age=0",
-                    "connection": "keep-alive",
-                    "content-length": "131",
-                    "content-type": "application/x-www-form-urlencoded",
-                    "host": "www12.honolulu.gov",
-                    "origin": "https://www12.honolulu.gov",
-                    "referer": "https://www12.honolulu.gov/specialplates/main/frmInquiry.asp?sFlag=search&sType=",
-                    "sec-ch-ua": '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
-                    "sec-ch-ua-mobile": "?0",
-                    "sec-ch-ua-platform": '"macOS"',
-                    "sec-fetch-dest": "document",
-                    "sec-fetch-mode": "navigate",
-                    "sec-fetch-site": "same-origin",
-                    "sec-fetch-user": "?1",
-                    "upgrade-insecure-requests": "1",
-                    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
-                }
-                data2 = {}
-                if len(word) == 1:
-                    data2 = {
-                        "txtLicense1": f"{word[0]}",
+            retries = 0
+            while retries < self._max_retries:
+                try:
+                    session = requests.Session()
+                    headers = {
+                        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                        "accept-encoding": "gzip, deflate, br",
+                        "accept-language": "en-US,en;q=0.9",
+                        "cache-control": "max-age=0",
+                        "connection": "keep-alive",
+                        "content-length": "131",
+                        "content-type": "application/x-www-form-urlencoded",
+                        "host": "www12.honolulu.gov",
+                        "origin": "https://www12.honolulu.gov",
+                        "referer": "https://www12.honolulu.gov/specialplates/main/frmInquiry.asp?sFlag=search&sType=",
+                        "sec-ch-ua": '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+                        "sec-ch-ua-mobile": "?0",
+                        "sec-ch-ua-platform": '"macOS"',
+                        "sec-fetch-dest": "document",
+                        "sec-fetch-mode": "navigate",
+                        "sec-fetch-site": "same-origin",
+                        "sec-fetch-user": "?1",
+                        "upgrade-insecure-requests": "1",
+                        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
+                    }
+                    data = {
+                        "txtLicense1": "",
                         "txtLicense2": "",
                         "txtLicense3": "",
                         "txtLicense4": "",
@@ -55,184 +57,113 @@ class Worker(multiprocessing.Process):
                         "txtPlate": f"{word}",
                         "txtPrev": "2"
                     }
-                if len(word) == 2:
-                    data2 = {
-                        "txtLicense1": f"{word[0]}",
-                        "txtLicense2": f"{word[1]}",
-                        "txtLicense3": "",
-                        "txtLicense4": "",
-                        "txtLicense5": "",
-                        "txtLicense6": "",
-                        "txtLicense7": "",
-                        "btnSubmit": "Search",
-                        "radType": "on",
-                        "txtPlate": f"{word}",
-                        "txtPrev": "2"
-                    }
-                if len(word) == 3:
-                    data2 = {
-                        "txtLicense1": f"{word[0]}",
-                        "txtLicense2": f"{word[1]}",
-                        "txtLicense3": f"{word[2]}",
-                        "txtLicense4": "",
-                        "txtLicense5": "",
-                        "txtLicense6": "",
-                        "txtLicense7": "",
-                        "btnSubmit": "Search",
-                        "radType": "on",
-                        "txtPlate": f"{word}",
-                        "txtPrev": "2"
-                    }
-                if len(word) == 4:
-                    data2 = {
-                        "txtLicense1": f"{word[0]}",
-                        "txtLicense2": f"{word[1]}",
-                        "txtLicense3": f"{word[2]}",
-                        "txtLicense4": f"{word[3]}",
-                        "txtLicense5": "",
-                        "txtLicense6": "",
-                        "txtLicense7": "",
-                        "btnSubmit": "Search",
-                        "radType": "on",
-                        "txtPlate": f"{word}",
-                        "txtPrev": "2"
-                    }
-                if len(word) == 5:
-                    data2 = {
-                        "txtLicense1": f"{word[0]}",
-                        "txtLicense2": f"{word[1]}",
-                        "txtLicense3": f"{word[2]}",
-                        "txtLicense4": f"{word[3]}",
-                        "txtLicense5": f"{word[4]}",
-                        "txtLicense6": "",
-                        "txtLicense7": "",
-                        "btnSubmit": "Search",
-                        "radType": "on",
-                        "txtPlate": f"{word}",
-                        "txtPrev": "2"
-                    }
-                response = session.post(url="https://www12.honolulu.gov/specialplates/main/frmInquiry.asp?sFlag=search&sType=", headers=headers2, data=data2)
-                if 'class="text-msg-blue">Congratulations!' not in str(response.text):
-                    print(colored("PLATE UNAVAILABLE:  " + word, 'red'))
-                else:
-                    print(colored("PLATE AVAILABLE:  " + word, 'green'))
-                    f = open("outputresults.txt", "a")
-                    f.write(word + "\n")
-                    f.close()
 
-            except:
-                self._job_queue.put(word)
+                    for character in range(len(word)):
+                        data[f"txtLicense{character + 1}"] = f"{word[character]}"
+
+                    response = session.post(
+                        url="https://www12.honolulu.gov/specialplates/main/frmInquiry.asp?sFlag=search&sType=",
+                        headers=headers,
+                        data=data,
+                        timeout=10
+                    )
+
+                    if response.status_code == 200:
+                        if 'class="text-msg-blue">Congratulations!' not in str(response.text):
+                            print(colored("PLATE UNAVAILABLE:  " + word, 'red'))
+                        else:
+                            print(colored("PLATE AVAILABLE:  " + word, 'green'))
+                            f = open("outputresults.txt", "a")
+                            f.write(word + "\n")
+                            f.close()
+                        break
+
+                except:
+                    retries += 1
+
+
+def generateCombinations(input_number):
+
+    # Used for combination generation
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    numbers = "0123456789"
+
+    # Creating a list of all 1-character combinations
+    if input_number == 1:
+        return [
+            *[a for a in alphabet],
+            *[b for b in alphabet]
+        ]
+
+    # Creating a list of all 3-character combinations
+    elif input_number == 2:
+        return [
+            *[a + b for a in alphabet for b in alphabet],
+            *[a + b for a in alphabet for b in numbers],
+            *[a + b for a in numbers for b in numbers]
+        ]
+
+    # Creating a list of all 3-letter combinations
+    elif input_number == 3:
+        return [a + b + c for a in alphabet for b in alphabet for c in alphabet]
+
+    # Creating a list of all 3-number combinations
+    elif input_number == 4:
+        return [a + b + c for a in numbers for b in numbers for c in numbers]
+
+    # Creating a list of all 3-letter words via GitHub scrape
+    elif input_number == 5:
+        return requests.get(
+            url="https://raw.githubusercontent.com/averywit/LicensePlateChecker/main/3letterwords.txt"
+        ).text.split("\n")
+
+    # Creating a list of all 4-letter words via GitHub scrape
+    elif input_number == 6:
+        return requests.get(
+            url="https://raw.githubusercontent.com/averywit/LicensePlateChecker/main/4letterwords.txt"
+        ).text.split("\n")
+
+    # Creating a list of all 5-letter words via GitHub scrape
+    elif input_number == 7:
+        return requests.get(
+            url="https://raw.githubusercontent.com/averywit/LicensePlateChecker/main/5letterwords.txt"
+        ).text.split("\n")
+
+    # Creating a list of all 3, 4, 5-letter repeater combinations
+    elif input_number == 8:
+        return [
+            *[a + a + a for a in alphabet],
+            *[a + a + a + a for a in alphabet],
+            *[a + a + a + a + a for a in alphabet]
+        ]
 
 
 if __name__ == '__main__':
+
     # On-screen input for desired checks
-    print("1 <- All 1 letter/character combinations")
+    print("1 <- All 1 character combinations")
     print("2 <- All 2 letter combinations")
     print("3 <- All 3 letter combinations")
-    print("4 <- All 3 letter words")
-    print("5 <- All 4 letter words")
-    print("6 <- All 5 letter words")
-    print("7 <- All 3 numbers")
-    print("8 <- All 3-4 letter repeaters")
+    print("4 <- All 3 number combinations")
+    print("5 <- All 3 letter word combinations")
+    print("6 <- All 4 letter word combinations")
+    print("7 <- All 5 letter word combinations")
+    print("8 <- All 3, 4, 5 letter repeater combinations")
     choice = int(input("Please enter what you want to check: "))
 
-    lines = []
-    random = 0
-    alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U",
-                "V", "W", "X", "Y", "Z"]
-    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    index = 0
-    # Creating an array of all the 1 letter/character combinations
-    if choice == 1:
-        for i in alphabet:
-            lines.insert(index, i)
-            index += 1
-        for i in numbers:
-            lines.insert(index, i)
-            index += 1
-    # Creating an array of all 2 letter combinations
-    if choice == 2:
-        for i in alphabet:
-            for j in alphabet:
-                lines.insert(index, i + j)
-                index += 1
-            for k in numbers:
-                lines.insert(index, i + k)
-                index += 1
-        for l in numbers:
-            for m in numbers:
-                lines.insert(index, l + m)
-                index += 1
-            for n in alphabet:
-                lines.insert(index, l + n)
-    # Creating an array of all 3 letter combinations
-    elif choice == 3:
-        for i in alphabet:
-            for j in alphabet:
-                for k in alphabet:
-                    lines.insert(index, i + j + k)
-                    index += 1
-    # Retrieving the list of all 3 letter words from Github scrape
-    elif choice == 4:
-        url = "https://raw.githubusercontent.com/averywit/LicensePlateChecker/main/3letterwords.txt"
-        r = requests.get(url)
-        for line in r.iter_lines():
-            if line:
-                lines.insert(index, str(line).strip("b'"))
-                index += 1
-    # Retrieving the list of all 4 letter words from Github scrape
-    elif choice == 5:
-        url = "https://raw.githubusercontent.com/averywit/LicensePlateChecker/main/4letterwords.txt"
-        r = requests.get(url)
-        for line in r.iter_lines():
-            if line:
-                stupidparse = str(line)
-                lines.insert(index, stupidparse[2:6])
-                index += 1
-
-    # Retrieving the list of all 5 letter words from Github scrape
-    elif choice == 6:
-        url = "https://raw.githubusercontent.com/averywit/LicensePlateChecker/main/5letterwords.txt"
-        r = requests.get(url)
-        for line in r.iter_lines():
-            if line:
-                stupidparse = str(line)
-                lines.insert(index, stupidparse[2:7])
-                index += 1
-
-    # Creating an array of all 3 + 4 number combinations
-    elif choice == 7:
-        for i in numbers:
-            for j in numbers:
-                for k in numbers:
-                    lines.insert(index, i + j + k)
-                    index += 1
-        for i in numbers:
-            for j in numbers:
-                for k in numbers:
-                    for l in numbers:
-                        lines.insert(index, i + j + k + l)
-                        index += 1
-
-    # Creating an array of all repeater combinations
-    elif choice == 8:
-        for i in alphabet:
-            lines.insert(index, i + i + i)
-            index += 1
-        for j in alphabet:
-            lines.insert(index, j + j + j + j)
-            index += 1
+    # Holds the combinations to check
+    combinations = generateCombinations(choice)
 
     jobs = []
-    job_queue = multiprocessing.Queue()
+    job_queue = Queue()
 
-    for i in range(10):
+    for i in range(25):
         p = Worker(job_queue)
         jobs.append(p)
         p.start()
 
-    for line in lines:
-        job_queue.put(line)
+    for combo in combinations:
+        job_queue.put(combo)
 
     for j in jobs:
         job_queue.put(None)
