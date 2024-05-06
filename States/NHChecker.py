67c8e27a713f65f7201eb58cc6502b889494b2eb
Avery Witherow <78763092+averywit@users.noreply.github.com>
2024-05-05 20:55:07 -0400

    Update NHChecker.py
    Added more code comments for fun!

--- States/NHChecker.py
+++ States/NHChecker.py
@@ -10,24 +10,43 @@ class Worker(threading.Thread):
 
     def __init__(self, job_queue, max_retries=10):
         super().__init__()
+        
+        # Assigns the job queue to the worker.
+        # This is filled with the combinations to check.
         self._job_queue = job_queue
+
+        # Sets the maximum retries (10) for failed requests.
         self._max_retries = max_retries
 
     def run(self):
+
+        # Runs worker until "None" is reached.
+        # None = No combinations are left to check.
         while True:
+
+            # Retreives a combination "word" from "job_queue".
             word = self._job_queue.get()
+
+            # Breaks loop when "None" is retrieved from "job_queue".
             if word is None:
                 break
 
             retries = 0
+
+            # Retry loop that makes multithreading happy.
+            # Most of the time, this will not loop. It is just for safety.
             while retries < self._max_retries:
                 try:
                     session = requests.Session()
+
+                    # Make the request to get the cookie needed for the check.
+                    # Timeout of 10 seconds incase of a stuck request.
                     session.get(
                         url="https://business.nh.gov/Platecheck/platecheck.aspx",
                         timeout=10
                     )
 
+                    # Data to complete the combination check.
                     data = {
                         "__LASTFOCUS": "",
                         "__VIEWSTATE": "/wEPDwUKMTEwMTE0OTU5Ng9kFgJmD2QWAgIBD2QWAgIBD2QWBAIBD2QWGAIJDxAPZBYCHghvbkNoYW5nZQUNQ2hhbmdlUGxhdGUoKWRkZAINDw8WAh4EVGV4dAUcU2VsZWN0IHBhc3NlbmdlciBwbGF0ZSB0eXBlOhYCHgVzdHlsZQUPZGlzcGxheTppbmxpbmU7ZAIPDxAPFgYeDURhdGFUZXh0RmllbGQFDVBsYXRlVHlwZVRleHQeDkRhdGFWYWx1ZUZpZWxkBQJJRB4LXyFEYXRhQm91bmRnFgQfAAURQ2hhbmdlUGFzc2VuZ2VyKCkfAgUPZGlzcGxheTppbmxpbmU7EBUPIS0tIFNlbGVjdCBQYXNzZW5nZXIgUGxhdGUgVHlwZSAtLShBbnRpcXVlIChpYW50aSkgICAgICAgICAgICAgICAgICAgICAgICAgKENvbnNlcnZhdGlvbiAoaWNwYXMgLSBNb29zZSBQbGF0ZSkgICAgICAoRGlzYWJsZWQgVmV0ZXJhbiAoaWR2ZXQpICAgICAgICAgICAgICAgIChGb3JtZXIgUE9XIChpZnBvdykgICAgICAgICAgICAgICAgICAgICAgKEhhbmRpY2FwcGVkIChpaGNhcCkgICAgICAgICAgICAgICAgICAgICAeSW5pdGlhbCBOYXRpb25hbCBHdWFyZCAoaW5nbmgpFUluaXRpYWwgUGFya3MgKGlzcHBzKSJJbml0aWFsIFBhcmtzIENvbnNlcnZhdGlvbiAoaXNwY3ApJEludGlpYWwgQWN0aXZlIER1dHkgTWlsaXRhcnkgKGlhZGFmKShQYXNzZW5nZXIgKGlwYXNzKSAgICAgICAgICAgICAgICAgICAgICAgKFBlYXJsIEhhcmJvciBTdXJ2aXZvciAoaXBoYnIpICAgICAgICAgICAoUHVycGxlIEhlYXJ0IChpbnB1cikgICAgICAgICAgICAgICAgICAgIChSZWd1bGFyIFZldGVyYW4gKGl2dmV0KSAgICAgICAgICAgICAgICAgKFN0cmVldCBSb2QgKGlzcm9kKSAgICAgICAgICAgICAgICAgICAgICAVDyEtLSBTZWxlY3QgUGFzc2VuZ2VyIFBsYXRlIFR5cGUgLS0FSUFOVEkFSUNQQVMFSURWRVQFSUZQT1cFSUhDQVAFSU5HTkgFSVNQUFMFSVNQQ1AFSUFEQUYFSVBBU1MFSVBIQlIFSU5QVVIFSVZWRVQFSVNST0QUKwMPZ2dnZ2dnZ2dnZ2dnZ2dnZGQCEQ8QDxYGHwMFDVBsYXRlVHlwZVRleHQfBAUCSUQfBWcWBB8ABRJDaGFuZ2VNb3RvcmN5Y2xlKCkfAgUNZGlzcGxheTpub25lOxAVBSEtLSBTZWxlY3QgUGFzc2VuZ2VyIFBsYXRlIFR5cGUgLS0oSGFuZGljYXAgTW90b3JjeWNsZSAoaWhtb3QpICAgICAgICAgICAgIChNb3RvcmN5Y2xlIChpbW90bykgICAgICAgICAgICAgICAgICAgICAgKFB1cnBsZSBIZWFydCBNb3RvcmN5Y2xlIChpcG1vdCkgICAgICAgICAoVmV0ZXJhbiBNb3RvcmN5Y2xlIChpdm1vdCkgICAgICAgICAgICAgIBUFIS0tIFNlbGVjdCBQYXNzZW5nZXIgUGxhdGUgVHlwZSAtLQVJSE1PVAVJTU9UTwVJUE1PVAVJVk1PVBQrAwVnZ2dnZ2RkAhMPEA8WBh8DBQ1QbGF0ZVR5cGVUZXh0HwQFAklEHwVnFgQfAAUSQ2hhbmdlQ29tbWVyY2lhbCgpHwIFDWRpc3BsYXk6bm9uZTsQFQMhLS0gU2VsZWN0IFBhc3NlbmdlciBQbGF0ZSBUeXBlIC0tKEFwcG9ydGlvbmVkIFZlaGljbGUgKGlhcHJvKSAgICAgICAgICAgICAoQ29tbWVyY2lhbCAoaWNvbW0pICAgICAgICAgICAgICAgICAgICAgIBUDIS0tIFNlbGVjdCBQYXNzZW5nZXIgUGxhdGUgVHlwZSAtLQVJQVBSTwVJQ09NTRQrAwNnZ2dkZAIVDxAPFgYfAwUNUGxhdGVUeXBlVGV4dB8EBQJJRB8FZxYEHwAFD0NoYW5nZVRyYWlsZXIoKR8CBQ1kaXNwbGF5Om5vbmU7EBUCIS0tIFNlbGVjdCBQYXNzZW5nZXIgUGxhdGUgVHlwZSAtLShUcmFpbGVyIChpdHJhaSkgICAgICAgICAgICAgICAgICAgICAgICAgFQIhLS0gU2VsZWN0IFBhc3NlbmdlciBQbGF0ZSBUeXBlIC0tBUlUUkFJFCsDAmdnZGQCFw8PZBYCHwIFDWRpc3BsYXk6bm9uZTtkAhkPDxYCHwEFFEVudGVyIHBsYXRlIHJlcXVlc3Q6FgIfAgUZZGlzcGxheTppbmxpbmU7Y29sb3I6cmVkO2QCGw8PFgIeCU1heExlbmd0aAIHFgIfAgUPZGlzcGxheTppbmxpbmU7ZAIdDw8WAh8BBRUoTWF4aW11bSBsZW5ndGggaXMgNykWAh8CBQ9kaXNwbGF5OmlubGluZTtkAh8PD2QWAh4Hb25DbGljawUTcmV0dXJuIFN1Ym1pdEZvcm0oKWQCIQ8PFgIfAQUVQ0FUIGlzIG5vdCBhdmFpbGFibGUhFgIfAgUPZGlzcGxheTppbmxpbmU7ZAICDw9kFgIfAgUNZGlzcGxheTpub25lOxYCAgUPDxYCHwEFBUlQQVNTZGRk/eQ2Fg+OlMt8Bz2cG7LZvKIwvSw=",
@@ -47,40 +66,56 @@ class Worker(threading.Thread):
                         "ctl00$cphMain$SubmitCommand": "Submit",
                         "ctl00$cphMain$JSTest": "1"
                     }
+
+                    # Make the request to complete the combination check.
+                    # Timeout of 10 seconds incase of a stuck request.
                     response = session.post(
                         url="https://business.nh.gov/Platecheck/platecheck.aspx",
                         data=data,
                         timeout=10
                     )
 
+                    # Successful request, continue to the next.
                     if response.status_code == 200:
+                        
+                        # The HTML below being in the request's response means it is available.
+                        # Record an available license plate combination.
                         if '<div id="ctl00_cphMain_ResultsDisplay" style="display:none;">' not in response.text:
                             print(colored("PLATE AVAILABLE:  " + word, 'green'))
                             f = open("outputresults.txt", "a")
                             f.write(word + "\n")
                             f.close()
+
+                        # The HTML above not being in the request's response means it is taken.
+                        # Do not record a taken license plate combination.
                         else:
                             print(colored("PLATE UNAVAILABLE:  " + word, 'red'))
+                        
+                        # Exits the retry loop because a successful check was completed.
                         break
 
+                    # Bad request, repeat it.
+                    # Any bad request will trigger the exception below.
+
+                # Increases the counter for total retries of checking the combination.
                 except:
                     retries += 1
 
 
 def generateCombinations(input_number):
 
-    # Used for combination generation
+    # Used for combination generation.
     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     numbers = "0123456789"
 
-    # Creating a list of all 1-character combinations
+    # Creating a list of all 1-character combinations.
     if input_number == 1:
         return [
             *[a for a in alphabet],
             *[b for b in alphabet]
         ]
 
-    # Creating a list of all 3-character combinations
+    # Creating a list of all 3-character combinations.
     elif input_number == 2:
         return [
             *[a + b for a in alphabet for b in alphabet],
@@ -88,39 +123,39 @@ def generateCombinations(input_number):
             *[a + b for a in numbers for b in numbers]
         ]
 
-    # Creating a list of all 3-letter combinations
+    # Creating a list of all 3-letter combinations.
     elif input_number == 3:
         return [a + b + c for a in alphabet for b in alphabet for c in alphabet]
 
-    # Creating a list of all 3-number combinations
+    # Creating a list of all 3-number combinations.
     elif input_number == 4:
         return [a + b + c for a in numbers for b in numbers for c in numbers]
 
-    # Creating a list of all 3-letter words via GitHub scrape
+    # Creating a list of all 3-letter words via GitHub scrape.
     elif input_number == 5:
         return requests.get(
             url="https://raw.githubusercontent.com/averywit/LicensePlateChecker/main/WordLists/3letterwords.txt"
         ).text.split("\n")
 
-    # Creating a list of all 4-letter words via GitHub scrape
+    # Creating a list of all 4-letter words via GitHub scrape.
     elif input_number == 6:
         return requests.get(
             url="https://raw.githubusercontent.com/averywit/LicensePlateChecker/main/WordLists/4letterwords.txt"
         ).text.split("\n")
 
-    # Creating a list of all 5-letter words via GitHub scrape
+    # Creating a list of all 5-letter words via GitHub scrape.
     elif input_number == 7:
         return requests.get(
             url="https://raw.githubusercontent.com/averywit/LicensePlateChecker/main/WordLists/5letterwords.txt"
         ).text.split("\n")
 
-    # Creating a list of all 6-letter words via GitHub scrape
+    # Creating a list of all 6-letter words via GitHub scrape.
     elif input_number == 8:
         return requests.get(
             url="https://raw.githubusercontent.com/averywit/LicensePlateChecker/main/WordLists/6letterwords.txt"
         ).text.split("\n")
 
-    # Creating a list of all 3, 4, 5, 6 repeater combinations
+    # Creating a list of all 3, 4, 5, 6 repeater combinations.
     elif input_number == 9:
         return [
             *[a + a + a for a in alphabet],
@@ -136,7 +171,7 @@ def generateCombinations(input_number):
 
 if __name__ == '__main__':
 
-    # On-screen input for desired checks
+    # On-screen input for desired checks.
     print("1 <- All 1 character combinations")
     print("2 <- All 2 letter combinations")
     print("3 <- All 3 letter combinations")
@@ -148,7 +183,7 @@ if __name__ == '__main__':
     print("9 <- All 3, 4, 5, 6 repeater combinations")
     choice = int(input("Please enter what you want to check: "))
 
-    # Holds the combinations to check
+    # Holds the combinations to check.
     combinations = generateCombinations(choice)
 
     jobs = []
