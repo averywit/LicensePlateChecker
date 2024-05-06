5f88c3191cc055b5d5876af6ce122c32aadd07a5
Avery Witherow <78763092+averywit@users.noreply.github.com>
2024-05-05 20:15:31 -0400

    Update FLChecker.py
    Added more code comments for fun!

--- States/FLChecker.py
+++ States/FLChecker.py
@@ -11,64 +11,99 @@ class Worker(threading.Thread):
 
     def __init__(self, job_queue, max_retries=10):
         super().__init__()
+
+        # Assigns the job queue to the worker.
+        # This is filled with the combinations to check.
         self._job_queue = job_queue
+
+        # Sets the maximum retries (10) for failed requests.
         self._max_retries = max_retries
 
     def run(self):
+
+        # Runs worker until "None" is reached.
+        # None = No combinations are left to check.
         while True:
-            word1 = self._job_queue.get()
-            if word1 is None:
+
+            # Retreives a combination "word" from "job_queue".
+            word = self._job_queue.get()
+
+            # Breaks loop when "None" is retrieved from "job_queue".
+            if word is None:
                 break
 
             retries = 0
+
+            # Retry loop that makes multithreading happy.
+            # Most of the time, this will not loop. It is just for safety.
             while retries < self._max_retries:
                 try:
                     session = requests.session()
+
+                    # Make the request to get the dynamic data needed for the check.
+                    # Timeout of 10 seconds incase of a bad proxy.
                     response = session.get(
                         url="https://services.flhsmv.gov/MVCheckPersonalPlate/",
                         timeout=10
                     )
 
+                    # Data to complete the combination check.
                     data = {
                         '__VIEWSTATE': re.search(r'<input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="([^"]*)" />', response.text).group(1),
                         '__VIEWSTATEGENERATOR': re.search(r'<input type="hidden" name="__VIEWSTATEGENERATOR" id="__VIEWSTATEGENERATOR" value="([^"]*)" />', response.text).group(1),
                         '__EVENTVALIDATION': re.search(r'<input type="hidden" name="__EVENTVALIDATION" id="__EVENTVALIDATION" value="([^"]*)" />', response.text).group(1),
-                        'ctl00$MainContent$txtInputRowOne': word1,
+                        'ctl00$MainContent$txtInputRowOne': word,
                         'ctl00$MainContent$btnSubmit': 'Submit'}
+
+                    # Make the request to complete the combination check.
+                    # Timeout of 10 seconds incase of a bad proxy.
                     response = session.post(
                         url="https://services.flhsmv.gov/MVCheckPersonalPlate/",
                         data=data,
                         timeout=10
                     )
 
+                    # Successful request, continue to the next.
                     if response.status_code == 200:
+                        
+                        # The HTML below being in the request's response means it is available.
+                        # Record an available license plate combination.
                         if '<span id="MainContent_lblOutPutRowOne" class="outputText" style="color: #0000a0; font-weight: bold">AVAILABLE</span>' in response.text:
-                            print(colored("PLATE AVAILABLE:  " + word1, 'green'))
+                            print(colored("PLATE AVAILABLE:  " + word, 'green'))
                             f = open("outputresults.txt", "a")
                             f.write(word1 + "\n")
                             f.close()
+
+                        # The HTML above not being in the request's response means it is taken.
+                        # Do not record a taken license plate combination.
                         else:
-                            print(colored("PLATE UNAVAILABLE:  " + word1, 'red'))
+                            print(colored("PLATE UNAVAILABLE:  " + word, 'red'))
+
+                        # Exits the retry loop because a successful check was completed.
                         break
 
+                    # Bad request, repeat it.
+                    # Any bad request will trigger the exception below.
+
+                # Increases the counter for total retries of checking the combination.
                 except:
                     retries += 1
 
 
 def generateCombinations(input_number):
 
-    # Used for combination generation
+    # Used for combination generation.
     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     numbers = "0123456789"
 
-    # Creating a list of all 1-character combinations
+    # Creating a list of all 1-character combinations.
     if input_number == 1:
         return [
             *[a for a in alphabet],
             *[b for b in alphabet]
         ]
 
-    # Creating a list of all 3-character combinations
+    # Creating a list of all 3-character combinations.
     elif input_number == 2:
         return [
             *[a + b for a in alphabet for b in alphabet],
@@ -76,39 +111,39 @@ def generateCombinations(input_number):
             *[a + b for a in numbers for b in numbers]
         ]
 
-    # Creating a list of all 3-letter combinations
+    # Creating a list of all 3-letter combinations.
     elif input_number == 3:
         return [a + b + c for a in alphabet for b in alphabet for c in alphabet]
 
-    # Creating a list of all 3-number combinations
+    # Creating a list of all 3-number combinations.
     elif input_number == 4:
         return [a + b + c for a in numbers for b in numbers for c in numbers]
 
-    # Creating a list of all 3-letter words via GitHub scrape
+    # Creating a list of all 3-letter words via GitHub scrape.
     elif input_number == 5:
         return requests.get(
             url="https://raw.githubusercontent.com/averywit/LicensePlateChecker/main/WordLists/3letterwords.txt"
         ).text.split("\n")
 
-    # Creating a list of all 4-letter words via GitHub scrape
+    # Creating a list of all 4-letter words via GitHub scrape.
     elif input_number == 6:
         return requests.get(
             url="https://raw.githubusercontent.com/averywit/LicensePlateChecker/main/WordLists/4letterwords.txt"
         ).text.split("\n")
 
-    # Creating a list of all 5-letter words via GitHub scrape
+    # Creating a list of all 5-letter words via GitHub scrape.
     elif input_number == 7:
         return requests.get(
             url="https://raw.githubusercontent.com/averywit/LicensePlateChecker/main/WordLists/5letterwords.txt"
         ).text.split("\n")
 
-    # Creating a list of all 6-letter words via GitHub scrape
+    # Creating a list of all 6-letter words via GitHub scrape.
     elif input_number == 8:
         return requests.get(
             url="https://raw.githubusercontent.com/averywit/LicensePlateChecker/main/WordLists/6letterwords.txt"
         ).text.split("\n")
 
-    # Creating a list of all 3, 4, 5, 6 repeater combinations
+    # Creating a list of all 3, 4, 5, 6 repeater combinations.
     elif input_number == 9:
         return [
             *[a + a + a for a in alphabet],
@@ -124,7 +159,7 @@ def generateCombinations(input_number):
 
 if __name__ == '__main__':
 
-    # On-screen input for desired checks
+    # On-screen input for desired checks.
     print("1 <- All 1 character combinations")
     print("2 <- All 2 letter combinations")
     print("3 <- All 3 letter combinations")
@@ -136,7 +171,7 @@ if __name__ == '__main__':
     print("9 <- All 3, 4, 5, 6 repeater combinations")
     choice = int(input("Please enter what you want to check: "))
 
-    # Holds the combinations to check
+    # Holds the combinations to check.
     combinations = generateCombinations(choice)
 
     jobs = []
