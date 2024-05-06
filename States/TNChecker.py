245f5bf8ad648dcfeed54602dbecad4d22e96944
Avery Witherow <78763092+averywit@users.noreply.github.com>
2024-05-05 21:05:51 -0400

    Update TNChecker.py
    Added more code comments for fun!

--- States/TNChecker.py
+++ States/TNChecker.py
@@ -10,19 +10,36 @@ class Worker(threading.Thread):
 
     def __init__(self, job_queue, max_retries=10):
         super().__init__()
+        
+        # Assigns the job queue to the worker.
+        # This is filled with the combinations to check.
         self._job_queue = job_queue
+
+        # Sets the maximum retries (10) for failed requests.
         self._max_retries = max_retries
 
     def run(self):
+
+        # Runs worker until "None" is reached.
+        # None = No combinations are left to check.
         while True:
+
+            # Retreives a combination "word" from "job_queue".
             word = self._job_queue.get()
+
+            # Breaks loop when "None" is retrieved from "job_queue".
             if word is None:
                 break
 
             retries = 0
+
+            # Retry loop that makes multithreading happy.
+            # Most of the time, this will not loop. It is just for safety.
             while retries < self._max_retries:
                 try:
                     session = requests.Session()
+                    
+                    # Header to complete the combination check.
                     header = {
                         "accept": "application/json, text/javascript, */*; q=0.01",
                         "accept-encoding": "gzip, deflate, br",
@@ -41,10 +58,15 @@ class Worker(threading.Thread):
                         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
                         "X-Requested-With": "XMLHttpRequest"
                     }
+                    
+                    # Data to complete the combination check.
                     data = {
                         "send[endpoint]": f"/personalizedplates/verifyplate/{word}/3210",
                         "send[type]": "GET"
                     }
+                    
+                    # Make the request to complete the combination check.
+                    # Timeout of 10 seconds incase of a stuck request.
                     response = session.post(
                         url="https://personalizedplates.revenue.tn.gov/static/api/api.php",
                         headers=header,
@@ -52,34 +74,47 @@ class Worker(threading.Thread):
                         timeout=10
                     )
 
+                    # Response status code 202 means it is available.
+                    # Record an available license plate combination.
                     if response.status_code == 200:
                         print(colored("PLATE AVAILABLE:  " + word, 'green'))
                         f = open("outputresults.txt", "a")
                         f.write(word + "\n")
                         f.close()
-                        break
-                    elif response.status_code == 422:
-                        print(colored("PLATE UNAVAILABLE:  " + word, 'red'))
+
+                        # Successful request, continue to the next.
                         break
 
+                    # Response status code 422 being in the request's response means it is taken.
+                    # Do not record a taken license plate combination.
+                    elif response.status_code == 422:
+                        print(colored("PLATE UNAVAILABLE:  " + word, 'red'))
+
+                        # Successful request, continue to the next.
+                        break
+
+                    # Bad request, repeat it.
+                    # Any bad request will trigger the exception below.
+
+                # Increases the counter for total retries of checking the combination.
                 except:
                     retries += 1
 
 
 def generateCombinations(input_number):
 
-    # Used for combination generation
+    # Used for combination generation.
     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     numbers = "0123456789"
 
-    # Creating a list of all 1-character combinations
+    # Creating a list of all 1-character combinations.
     if input_number == 1:
         return [
             *[a for a in alphabet],
             *[b for b in alphabet]
         ]
 
-    # Creating a list of all 3-character combinations
+    # Creating a list of all 3-character combinations.
     elif input_number == 2:
         return [
             *[a + b for a in alphabet for b in alphabet],
@@ -87,39 +122,39 @@ def generateCombinations(input_number):
             *[a + b for a in numbers for b in numbers]
         ]
 
-    # Creating a list of all 3-letter combinations
+    # Creating a list of all 3-letter combinations.
     elif input_number == 3:
         return [a + b + c for a in alphabet for b in alphabet for c in alphabet]
 
-    # Creating a list of all 3-number combinations
+    # Creating a list of all 3-number combinations.
     elif input_number == 4:
         return [a + b + c for a in numbers for b in numbers for c in numbers]
 
-    # Creating a list of all 3-letter words via GitHub scrape
+    # Creating a list of all 3-letter words via GitHub scrape.
     elif input_number == 5:
         return requests.get(
             url="https://raw.githubusercontent.com/averywit/LicensePlateChecker/main/WordLists/3letterwords.txt"
         ).text.split("\n")
 
-    # Creating a list of all 4-letter words via GitHub scrape
+    # Creating a list of all 4-letter words via GitHub scrape.
     elif input_number == 6:
         return requests.get(
             url="https://raw.githubusercontent.com/averywit/LicensePlateChecker/main/WordLists/4letterwords.txt"
         ).text.split("\n")
 
-    # Creating a list of all 5-letter words via GitHub scrape
+    # Creating a list of all 5-letter words via GitHub scrape.
     elif input_number == 7:
         return requests.get(
             url="https://raw.githubusercontent.com/averywit/LicensePlateChecker/main/WordLists/5letterwords.txt"
         ).text.split("\n")
 
-    # Creating a list of all 6-letter words via GitHub scrape
+    # Creating a list of all 6-letter words via GitHub scrape.
     elif input_number == 8:
         return requests.get(
             url="https://raw.githubusercontent.com/averywit/LicensePlateChecker/main/WordLists/6letterwords.txt"
         ).text.split("\n")
 
-    # Creating a list of all 3, 4, 5, 6 repeater combinations
+    # Creating a list of all 3, 4, 5, 6 repeater combinations.
     elif input_number == 9:
         return [
             *[a + a + a for a in alphabet],
@@ -135,7 +170,7 @@ def generateCombinations(input_number):
 
 if __name__ == '__main__':
 
-    # On-screen input for desired checks
+    # On-screen input for desired checks.
     print("1 <- All 1 character combinations")
     print("2 <- All 2 letter combinations")
     print("3 <- All 3 letter combinations")
@@ -147,7 +182,7 @@ if __name__ == '__main__':
     print("9 <- All 3, 4, 5, 6 repeater combinations")
     choice = int(input("Please enter what you want to check: "))
 
-    # Holds the combinations to check
+    # Holds the combinations to check.
     combinations = generateCombinations(choice)
 
     jobs = []
